39a27c55a4c6ed1c1004fbafdf68965f65bbf801
Robert Fidytek <fidytek@wp.pl>
2017-11-27 11:05:00 +0100

    drugi commit

---
+++ src/newpythonproject1.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Robert"
+__date__ = "$2017-11-26 17:15:27$"
+
+if __name__ == "__main__":
+    print ("Hello World")
